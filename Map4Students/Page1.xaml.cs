e03d9704cc4eed499aa6bb7342962134bd2c9644
13870 <41979323+13870@users.noreply.github.com>
2018-09-12 11:22:48 +0800

    Add files via upload

---
+++ Map4Students/Page1.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Map4Students
+{
+    /// <summary>
+    /// Interaction logic for Page2.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private string Listchecker(List<string> list, string value, List<string> list2, string value2)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] == value && list2[i] == value2)
+                {
+                    return "Yes";
+                }
+            }
+            return "No";
+        }
+
+        private void buttonclick1(object sender, RoutedEventArgs e)
+        {
+            List<string> UserList = new List<string> {"13870", "1013414", "1020095"};
+            List<string> PassList = new List<string> { "101", "8008", "2" };
+            string inputUser = InputTextBox1.Text;
+            string inputPass = InputTextBox2.Text;
+            var checker = Listchecker(UserList, inputUser, PassList, inputPass);
+
+            if (checker == "Yes")
+            {
+                Page2 page2 = new Page2();
+                this.NavigationService.Navigate(page2);
+            }
+            else
+            {
+                MessageBox.Show("                                                  Incorrect Sudent ID Or Password                                                  ");
+                InputTextBox1.Text = "";
+                InputTextBox2.Text = "";
+            }
+           
+        }
+    }
+}
---
+++ Map4Students/Page2.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SQLite;
+
+namespace Map4Students
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        public Page2()
+        {
+            InitializeComponent();
+
+            SQLiteConnection conn = new SQLiteConnection("Data Source=TimeTabledb.db;Version=3;");
+            conn.Open();
+
+
+            conn.Close();
+        }
+
+        private void Go_Back(object sender, RoutedEventArgs e)
+        {
+            Page1 page1 = new Page1();
+            this.NavigationService.Navigate(page1);
+        }
+
+        private void Button3(object sender, RoutedEventArgs e)
+        {
+            MapPage mappage = new MapPage();
+            this.NavigationService.Navigate(mappage);
+        }
+    }
+}
