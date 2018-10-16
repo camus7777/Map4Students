b1be70992cbe5a1d28d95437785c60493ef9f89a
13870 <41979323+13870@users.noreply.github.com>
2018-10-16 14:42:44 +0800

    Add files via upload

---
+++ MapPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Map4Students
+{
+    /// <summary>
+    /// Interaction logic for MapPage.xaml
+    /// </summary>
+    public partial class MapPage : Page
+    {
+        public MapPage()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void ButtonMapPageBack(object sender, RoutedEventArgs e)
+        {
+            Page2 page2 = new Page2();
+            this.NavigationService.Navigate(page2);
+        }
+    }
+}
